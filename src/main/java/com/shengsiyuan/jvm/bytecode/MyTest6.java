caf93f9297cebba77df3cf2c8a1abacb0425d53b
MrLin <414048771@qq.com>
2020-04-16 21:58:15 +0800

    commit:p55已学完，了解类基于内存和寄存器的不同指令集之间的区别

--- src/main/java/com/shengsiyuan/jvm/bytecode/MyTest6.java
+++ src/main/java/com/shengsiyuan/jvm/bytecode/MyTest6.java
@@ -1,9 +1,48 @@
 package com.shengsiyuan.jvm.bytecode;
 
+/**
+ * 方法的动态分派
+ *
+ * 方法的动态分派涉及到一个重要概念：方法接收者。
+ *
+ * invokevirutal字节码指令的多态查找流程
+ *
+ * 比较方法重载（overload）与方法重写（overwrite）,我们可以得到这样的结论
+ * 方法重载是静态的，是编译期行为；方法重写是动态的，是运行期行为。
+ */
 public class MyTest6 {
 
     public static void main(String[] args) {
+        Fruit apple = new Apple();
+        Fruit orange = new Orange();
 
-        System.out.println(1);
+        apple.test();
+        orange.test();
+
+        apple = new Orange();
+        apple.test();
+    }
+}
+
+class Fruit {
+
+    public void test() {
+        System.out.println("Fruit");
+    }
+}
+
+class Apple extends Fruit {
+
+    @Override
+    public void test() {
+        System.out.println("Apple");
+    }
+}
+
+class Orange extends Fruit {
+
+    @Override
+    public void test() {
+        System.out.println("Orange");
     }
 }
