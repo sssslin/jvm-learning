673e249b9557a531f36e973484095e126b1761f2
sssslin <414048771@qq.com>
2020-05-06 08:27:32 +0800

    commit:gc

---
+++ src/main/java/com/shengsiyuan/jvm/gc/MyTest1.java
@@ -0,0 +1,15 @@
+package com.shengsiyuan.jvm.gc;
+
+public class MyTest1 {
+
+    public static void main(String[] args) {
+        int size = 1024 * 1024;
+
+        byte[] myAlloc1 = new byte[2 * size];
+        byte[] myAlloc2 = new byte[2 * size];
+        byte[] myAlloc3 = new byte[3 * size];
+//        byte[] myAlloc4 = new byte[2 * size];
+
+        System.out.println("hello world");
+    }
+}
